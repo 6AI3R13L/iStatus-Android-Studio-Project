4447ffe55628dd4aff691e35b7218866a107f87c
Gabriel Laniewski <laniewskigabriel@gmail.com>
2017-04-05 01:19:22 -0400

    Text in the text field, when clicked on, now disappears.                   Other minor fixes.

--- app/src/main/java/com/glaniewski/com/istatusarduinoproject/SecondActivity.java
+++ app/src/main/java/com/glaniewski/com/istatusarduinoproject/SecondActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
 
 /**
  * Created by pc on 3/28/2017.
@@ -26,7 +28,17 @@ public class SecondActivity extends MainActivity {
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.infoCard);
-        textView.setText("Sign in as: " + name);
+        textView.setText("Signed in as: " + name);
 
+        final Button button = (Button) findViewById(R.id.display);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.phrase);
+
+                Toast.makeText(getApplicationContext(), "Sent to display", Toast.LENGTH_SHORT).show();
+                editText.setText("");
+            }
+        });
     }
 }
